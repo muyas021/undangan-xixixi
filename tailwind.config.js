a20e039252d99ce1429feee054aab363814c3a4a
Muyas <muyas021@gmail.com>
2022-11-18 13:26:22 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      maxWidth: {
+        480: "30rem",
+      },
+      maxHeight: {
+        864: "54rem",
+      },
+      colors: {
+        sage: {
+          200: "#D5D9C7",
+          500: "#848B6C",
+          700: "#565E39",
+        },
+      },
+      screens: {
+        andro: "420px",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
